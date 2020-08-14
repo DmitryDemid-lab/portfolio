51252506354b88106680444c88dcbe8dff9716ff
DmitryDemid <dimon.demid15@gmail.com>
2020-08-13 21:16:19 -0400

    Updates

---
+++ precache-manifest.a417e6c7f3f1366f2b49d372f23903e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d5dc293db05277feab9c93a272184d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b653d07f0bfc76e7531d",
+    "url": "/portfolio/static/css/main.0d11f275.chunk.css"
+  },
+  {
+    "revision": "38533779a91fa64cd0ef",
+    "url": "/portfolio/static/js/2.95d246c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.95d246c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b653d07f0bfc76e7531d",
+    "url": "/portfolio/static/js/main.b972c0c1.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "01b50a3dab02364a8e95f0ef9d4e6ba5",
+    "url": "/portfolio/static/media/socialNetwork.01b50a3d.png"
+  },
+  {
+    "revision": "df6faf8db34e4df7b81619c282218c7e",
+    "url": "/portfolio/static/media/to-do-list-app.df6faf8d.png"
+  }
+]);
