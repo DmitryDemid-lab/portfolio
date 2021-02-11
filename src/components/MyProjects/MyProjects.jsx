236c200265d79298f235a38bff3c4991ee806802
DmitryDemid <dimon.demid15@gmail.com>
2021-02-10 23:54:35 -0500

    Adding new project

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -6,6 +6,7 @@ import Project from './Project/Project';
 import todoLogo from '../../assets/images/projects/to-do-list-app.png'
 import onlineStoreLogo from '../../assets/images/projects/ONLINE-STORE.jpg'
 import socialNetworkLogo from '../../assets/images/projects/socialNetwork.png'
+import onlineTheatreLogo from '../../assets/images/projects/online-theatre.png'
 import Fade from "react-reveal/Fade";
 
 function MyProjects(props) {
@@ -18,12 +19,18 @@ function MyProjects(props) {
     const onlineStoreBG = {
         backgroundImage: `url(${onlineStoreLogo})`
     }
+    const onlineTheatreBG = {
+        backgroundImage: `url(${onlineTheatreLogo})`
+    }
 
     const todoDescription = 'TodoList is smart task list for everyday use. No matter who you are and what you do - you will be better organized! Features: authorization (login, logout), creating, deleting todolists and tasks for it, updating titles of todolists and tasks (by double clicking on the title). Task status might be changed, there is filtration on tasks statuses.\n ' + 'STACK: ReactJs, Redux, TS, REST (axios), TDD, Jest, Css, MaterialUI, Formic'
 
     const socialNetworkDescription = 'A big project with aim to improve my skills. Features: Authorization (login, logout), uploading new user’s avatar. possibility of changing user information (by pen button) and status (by double clicking on the status). Also you can see all users in this social network (users tabpage in the menu), by clicking on any user you will see his profile and ability to add him into your friends list (follow) and deleting from friends list (unfollow)\n' + 'STACK: ReactJS, Redux, ReduxForm, TS, Css, Jest, TDD, REST (axios)'
 
     const onlineStoreDescription = 'A small online store with such features as: Adding items to cart, deleting items from cart, increasing count of items in cart, calculating finally price. Also you are able to place your order (forming JSON with information about customer and items from his order)\n' +
+        'STACK: ReactJs, Redux Toolkit, TypeScript, Firebase, REST (axios), Formic, TDD, Jest, Storybook, MaterialUI, SASS\n'
+
+    const onlineTheatreDescription = 'A small online store with such features as: Adding items to cart, deleting items from cart, increasing count of items in cart, calculating finally price. Also you are able to place your order (forming JSON with information about customer and items from his order)\n' +
         'STACK: ReactJs, Redux Toolkit, TypeScript, Firebase, REST (axios), Formic, TDD, Jest, Storybook, MaterialUI, SASS\n'
 
     return (
@@ -44,6 +51,10 @@ function MyProjects(props) {
                             <Project style={onlineStoreBG} projectTitle={'Online Sore'}
                                      projectDescription={onlineStoreDescription}
                                      projectUrl={'https://online-store-1600b.web.app/'}
+                            />
+                            <Project style={onlineTheatreBG} projectTitle={'Online Theatre'}
+                                     projectDescription={onlineTheatreDescription}
+                                     projectUrl={'https://online-theatre.web.app/'}
                             />
                         </div>
                     </div>
